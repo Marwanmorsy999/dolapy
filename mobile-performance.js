c2e10dca572ca1a10b8ac47e3913b32330fe37ab
Marawan morsy <m.2396@nu.edu.eg>
2026-09-10 20:45:45 +0300

    prefer fp16 segmentation for cleaner garment masks

--- mobile-performance.js
+++ mobile-performance.js
@@ -6,7 +6,7 @@ window.DolapyPerformance={
   mobile:LOW_END||/Mobile/i.test(navigator.userAgent),
   lowPower:LOW_END||slowNetwork,
   imageSize(){return this.lowPower?640:896},
-  segmentationModel(){return this.lowPower?'isnet_quint8':'isnet_fp16'},
+  segmentationModel(){return 'isnet_fp16'},
   classifierOptions(){return navigator.gpu&&!this.lowPower?{device:'webgpu',dtype:'fp16'}:{device:'wasm',dtype:'q8'}},
   shouldRunSecondPass(score){return Number(score||0)<(this.lowPower?.32:.42)},
   compositeLimit(){return this.lowPower?4:6}
