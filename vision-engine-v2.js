dc544728d3cde93833ce4bffba3eed96c3869f2f
Marawan morsy <m.2396@nu.edu.eg>
2026-09-14 05:02:33 +0300

    chore: remove obsolete vision engine v2

--- vision-engine-v2.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  'use strict';
-  if (document.querySelector('script[data-dolapy-vision-v3]')) return;
-  const script = document.createElement('script');
-  script.src = '/vision-engine-v3.js';
-  script.defer = true;
-  script.setAttribute('data-dolapy-vision-v3', 'true');
-  document.head.appendChild(script);
-})();
